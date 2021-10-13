1316b3618b7af3424874a27240c702d25133765d
Doo <endufsla20@naver.com>
2021-10-13 16:10:07 +0900

    commit

---
+++ src/test/java/com/example/juice/JuiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
